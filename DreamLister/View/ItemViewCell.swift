37ab580912f937a3f47e0f9a84513ebd18f5b35f
Gteoharov <Gteoharov@users.noreply.github.com>
2018-07-18 22:09:14 +0300

    preparing tableView

---
+++ DreamLister/View/ItemViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemViewCell.swift
+//  DreamLister
+//
+//  Created by Georgi Teoharov on 18.07.18.
+//  Copyright © 2018 Georgi Teoharov. All rights reserved.
+//
+
+import UIKit
+
+class ItemViewCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+}
